50fb8828f1d927e4c1a5cd48dbed78e36e049409
Jansen Felipe <jansen.felipe@gmail.com>
2018-07-20 09:24:27 -0300

    Add headers + fix server

--- src/Providers/FSistProvider.php
+++ src/Providers/FSistProvider.php
@@ -8,7 +8,6 @@ use JansenFelipe\NFeGratis\NFe;
 
 class FSistProvider implements ProviderContract
 {
-
     /**
      * @param HttpClientContract $client
      * @return array
@@ -17,7 +16,7 @@ class FSistProvider implements ProviderContract
     {
         $usuarioID = rand(100000000, 999999999);
 
-        $response = $client->get('https://server7.fsist.com.br/baixarxml.ashx?m=WEB&UsuarioID='.$usuarioID.'&cte=0&pub=&com=&t=captcha');
+        $response = $client->get('https://server9.fsist.com.br/baixarxml.ashx?m=WEB&UsuarioID='.$usuarioID.'&cte=0&pub=&com=&t=captcha');
 
         $captchaBase64 = base64_encode($response->getBody());
 
@@ -32,9 +31,18 @@ class FSistProvider implements ProviderContract
      */
     public function getNFe($key, $params = [], HttpClientContract $client)
     {
+        $client->setHeaders([
+            'Connection: keep-alive',
+            'Upgrade-Insecure-Requests: 1',
+            'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Referer: https://www.fsist.com.br/',
+            'Accept-Language: pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6,it;q=0.5'
+        ]);
+
         $usuarioID = $params['usuarioID'];
 
-        $response = $client->get("https://server2.fsist.com.br/baixarxml.ashx?m=WEB&UsuarioID=$usuarioID&cte=0&t=xmlsemcert&chave=$key");
+        $response = $client->get("https://server9.fsist.com.br/baixarxml.ashx?m=WEB&UsuarioID=$usuarioID&cte=0&t=xmlsemcert&chave=$key");
 
         return $response->getBody();
     }
